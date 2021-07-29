f8c52bbfddc0564afa3603be571a8c4da2e153d5
Dhanya Paulose <dhanyapaulose20@gmail.com>
2021-07-29 10:44:46 +0530

    final

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import './LoginForm.css';
+import React from "react";
+import "./LoginForm.css";
 
-function LoginForm(){
-    return(
-        <form>
-            <div className="form-inner">
-                <h2>Login</h2>
-                <div className="form-group">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" id="email"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password"/>
-                </div>
-                <input type="submit" value="LOGIN"/>
-            </div>
-
-        </form>
-    )
+function LoginForm() {
+  return (
+    <form>
+      <div className="form-inner">
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+           <input type="password" name="password" id="password" />
+        </div>
+        <input type="submit" value="LOGIN" />
+      </div>
+    </form>
+  );
 }
 
 export default LoginForm;
